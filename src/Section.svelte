d9716fb6bda3256df60108d639bd72f40a22e59f
Mattias Holmlund <mattias@holmlund.se>
2020-05-06 22:02:41 +0200

    Start moving css out of components

--- src/Section.svelte
+++ src/Section.svelte
@@ -4,31 +4,6 @@
   export { className as class };
 </script>
 
-<style>
-  div.section {
-    width: 100%;
-    height: 100%;
-    background-size: 100% 100%;
-    padding-left: 1em;
-  }
-
-  div.d2 {
-    height: 100%;
-    position: relative;
-  }
-
-  div.d3 {
-    margin: 0;
-    position: absolute;
-    top: 60%;
-    transform: translateY(-50%);
-  }
-
-  h1 {
-    margin-top: 0;
-  }
-</style>
-
 <div class="section {className}">
   <div class="d2">
     <div class="d3">
